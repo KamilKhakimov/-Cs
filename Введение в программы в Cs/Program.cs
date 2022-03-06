381ef6386157e306f49aa3ec291a8e5220a76104
Kamil Khakimov <kamil.khakimov.03@mail.ru>
2022-03-06 19:09:37 +0200

    Добавьте файлы проекта.

---
+++ Введение в программы в Cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Введение_в_программы_в_Cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Kamil";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
